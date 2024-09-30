b5027932ad0a1dbcc1564e0bd7b3bc7187c9dd4d
sravanthi-99 <parisesravanthi0@gmail.com>
2024-09-30 12:24:00 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,10 @@
+//sum of 2 numbers
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter values of a,b:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of a and b:%d",c);
+}
