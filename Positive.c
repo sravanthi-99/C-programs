ae2a0c9f25478c1fde036d2c981bb9d2d12a146b
sravanthi-99 <parisesravanthi0@gmail.com>
2024-10-25 11:57:58 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,13 @@
+//check number is positive
+#include<stdio.h>
+int main() 
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("Positive");
+    }
+    return 0;
+}
