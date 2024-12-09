84a8431427270f7d1b76e2c0bed2874b90c85a9e
sravanthi-99 <parisesravanthi0@gmail.com>
2024-12-09 12:24:21 +0530

    Create num1ton.c

---
+++ num1ton.c
@@ -0,0 +1,12 @@
+//print all numbers from 1 to n
+#include<stdio.h>
+int main() 
+{
+    int i,n;
+    scanf("%d", &n);
+    for(i=1;i<=n;++i)
+    {
+        printf("%d ", i);
+    }
+    return 0;
+}
