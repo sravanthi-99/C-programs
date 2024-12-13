8a7183d68806c2d421db34f1e87bca7fa1452f47
sravanthi-99 <parisesravanthi0@gmail.com>
2024-12-13 11:41:59 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,15 @@
+//reverse of given number
+#include<stdio.h>
+int main() 
+{
+    int n, rem, rev=0;
+    scanf("%d", &n);
+    while(n>0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("Reverse:%d",rev);
+    return 0;
+}
