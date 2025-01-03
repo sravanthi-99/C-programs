6e777c610bbeb2251a40c462de260fb74ed65043
sravanthi-99 <parisesravanthi0@gmail.com>
2025-01-03 12:06:03 +0530

    Create pattern9.c

---
+++ pattern9.c
@@ -0,0 +1,20 @@
+/*To print a pattern like 
+*	*	*	*	*	
+*	*	*	*	
+*	*	*	
+*	*	
+*	*/
+#include<stdio.h>
+int main()
+{
+    int i,j,n='A';
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
