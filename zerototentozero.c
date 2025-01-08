3a8b1b65e6b3d760cbd47f451dcbe7ba21f82f8e
sravanthi-99 <parisesravanthi0@gmail.com>
2025-01-08 13:50:00 +0530

    Create zerototentozero.c

---
+++ zerototentozero.c
@@ -0,0 +1,16 @@
+//prime numbers from 0 to 10 and 10 to 0
+#include<stdio.h>
+void main()
+{
+    int i=0,j=10,n=10;
+    while(i<=10)
+    {
+        printf("\n%d\n",i++);
+        
+    }
+    while(j>=0)
+    {
+        printf("\n%d\n",j--);
+        
+    }
+}
