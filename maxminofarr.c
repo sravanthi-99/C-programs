807a732d2d1ed364739446ef7ece66a0d7ec156e
sravanthi-99 <parisesravanthi0@gmail.com>
2025-01-08 16:52:24 +0530

    Create maxminofarr.c

---
+++ maxminofarr.c
@@ -0,0 +1,27 @@
+//maximum and minimum element in an array
+#include<stdio.h>
+int main() 
+{
+    int a[10],max, min,i, n;
+    float avg;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    max=a[0];
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    printf("%d\n%d",max,min);
+    return 0;
+}
