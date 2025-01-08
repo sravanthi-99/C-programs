466b51da1a8870793ba307dbf132fbb6bfc1ea2b
sravanthi-99 <parisesravanthi0@gmail.com>
2025-01-08 13:56:54 +0530

    Update zerototentozero.c

--- zerototentozero.c
+++ zerototentozero.c
@@ -1,6 +1,6 @@
 //prime numbers from 0 to 10 and 10 to 0
 #include<stdio.h>
-void main()
+int main()
 {
     int i=0,j=10,n=10;
     while(i<=10)
@@ -11,6 +11,6 @@ void main()
     while(j>=0)
     {
         printf("\n%d\n",j--);
-        
     }
+    return 0;
 }
